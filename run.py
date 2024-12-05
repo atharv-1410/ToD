5cde7873d4f6c555c726fe7a528c5a9fe3469779
atharv-1410 <atharvdeshpande92@gmail.com>
2024-12-05 19:50:09 +0530

    Updated run.py for Render deployment

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import os
 from todo_app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    # Get the port from the environment variable or use 5000 as a default
+    port = int(os.environ.get("PORT", 5000))
+    
+    # Run the app with host="0.0.0.0" so it can be accessed from outside the container
+    app.run(host="0.0.0.0", port=port)
